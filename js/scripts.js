7bcad572c9016b2005561a4776f03ce688d25e10
SebastianJ <sebastian.emerson1@gmail.com>
2016-10-31 14:09:24 +0000

    currency exchange api now works and will display a comparison between a base currency and a chosen foreign currency

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
 "use strict";
 
 var holidayFormDataOne = {
-	travelCost: 0,
-	accomodationCost: 0,
-	foodCost: 0,
-	otherCost:0
-
+		generalInfo: {
+			holidayTitle: 0
+		},
+		holidayCosts: {
+			travelCost: 0,
+			accomodationCost: 0,
+			foodCost: 0,
+			otherCost:0
+		}
 };
+
 
 var appController ={
 	initApp: function(){
 		this.clickEvents();
-	},
-	display: function(){
-
+		currencyOptionsXhrRequest.makeRequest('js/countries.json');
+		
 	},
 	clickEvents: function(){
 		var self = this;
@@ -24,9 +28,13 @@ var appController ={
 				}
 				if (el.target.classList.contains('changeFormButton')) {
 					self.updateCSS(el);
-				}; 
+				}
+				if(el.target.classList.contains('addComparisonCurrency')){
+					var baseCurrency = document.getElementById("baseCurrencyList").value;
+					self.updateCurrencyExData(baseCurrency);
+				} 
 
-			});
+			});	 
 	},
 	updateHolidayForm: function(el){
 		var findChildren = el.target.parentElement.children;
@@ -48,17 +56,23 @@ var appController ={
 		var trimmedInputBoxValue = inputBoxValue.trim()
 
 		if(el.target.parentElement.className === "cost"){
-			trimmedInputBoxValue = parseInt(trimmedInputBoxValue, 10);
-			resultsText.innerHTML = "&#xa3; "  + trimmedInputBoxValue;
+			if (isNaN(inputBoxValue)){
+	        	document.getElementById('nanWarning').innerHTML = "*You must enter a number";
+	        	return;
+	    	} else {
+				trimmedInputBoxValue = parseFloat(trimmedInputBoxValue, 10);
+				resultsText.innerHTML = "&#xa3; "  + trimmedInputBoxValue;
+				document.getElementById('nanWarning').innerHTML = "";
+
+			}
 		} else{
 			resultsText.innerHTML = trimmedInputBoxValue;
 		}
 		
 		
-		this.updateFormData(parentElementId, trimmedInputBoxValue);
+		this.updateFormDataObj(parentElementId, trimmedInputBoxValue);
 		this.updateCSS(el); //passed in HTMLCollection to update CSS classes
 		
-		// this.updateData(trimmedInputBoxValue);
 		},
 	updateCSS: function(el){
 		var findChildren = el.target.parentElement.children;
@@ -74,8 +88,7 @@ var appController ={
 					findChildren[i].className = 'changeFormButton'  + ' ' + 'show';
 				}
 			}
-		}//To fix issue with updating change button, change all to find classlist
-		//you'll need to make the classnames be equal to original + new each time so it removes old classes.
+		}
 		function changeItemCSS(){
 			for (var i = 0; i < findChildren.length; i++) {
 				if(findChildren[i].classList.contains('inputBox')){
@@ -98,11 +111,16 @@ var appController ={
 		}
 
 	},
-	updateFormData: function(id, itemCostsArray){
-		//if statement to handle whether form1 or form 2 
-		holidayFormDataOne[id] = itemCostsArray;
-		console.log(holidayFormDataOne);
+	updateFormDataObj: function(id, itemCostsArray){
+		//if statement to handle whether form1 or form 2
+
+		if (id === 'holidayTitle' || id === 'holidayLength'){
+			holidayFormDataOne.generalInfo[id] = itemCostsArray;
+		} else{
+			holidayFormDataOne.holidayCosts[id] = itemCostsArray;
+		}
 		this.displayDataTotals();
+
 	},
 	displayDataTotals: function(){
 		//Need to account for formdata2.  This function should update
@@ -111,15 +129,15 @@ var appController ={
 		//find element where results will be displayed
 		var costsTotalLabel = document.getElementById('totalSpend');
 
-		//loop through values of each key in form - prep for reduce.
-		var formOne = holidayFormDataOne;
+		//loop through values of each key in holidayCosts section
+		//of the form - prep for reduce.
+		var formOne = holidayFormDataOne.holidayCosts;
 		var costsArrayOne = [];
 
 		for(var key in formOne) {
     	costsArrayOne.push(formOne[key]);
 		}
 
-		
 		//reduce to total cost and display results
 		var totalCosts;
 
@@ -132,9 +150,81 @@ var appController ={
 		reduceCosts(costsArrayOne);
 
 		costsTotalLabel.innerHTML = "&#xa3; "  + totalCosts;
+	},
+	updateCurrencyExData: function(baseCurrencyCode){// make this click event on form
+		var currencyBaseUrl = "https://api.fixer.io/latest?base=";
+		currencyBaseUrl += baseCurrencyCode;
+		currencyDataXhrRequest.makeRequest(currencyBaseUrl);
 	}
-
 };
+//get currency exchange data
+var currencyDataXhrRequest = {
+  makeRequest: function(url){
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Accept", "text/json");
+    xhr.onreadystatechange = this.receiveResponse;
+    xhr.send();
+  },
+  receiveResponse: function(){ //'this' is XMLHttpRequest
+    if (this.readyState == 4 && this.status == 200) {
+    console.log(this.statusText)
+    var requestedItems = JSON.parse(this.responseText);
+    currencyDataXhrRequest.formatResponse(requestedItems);
+  	}
+  },
+  formatResponse: function(el){
+  	var comparisonCurrency = document.getElementById("comparisonCurrencyList").value;
+   	var displayExRate = document.getElementById("currencyExResults");
+   	var ratesObj = el.rates
+   	var comparisonCurrencyResult;
+   		for (var key in ratesObj) {
+  			if (ratesObj.hasOwnProperty(key)) {
+  				if (key === comparisonCurrency) {
+  					comparisonCurrencyResult = ratesObj[key] + " " + key;
+  				};
+  				if (comparisonCurrency === el.base) {
+  					   	displayExRate.innerHTML = "Warning: You are comparing the same currency";
+;						return;
+  				};
+  				
+  			}
+  	}
+
+   	displayExRate.innerHTML = '1 ' + el.base + ' = ' + comparisonCurrencyResult;
+}
+};
+
+//Get Currency Options
+var currencyOptionsXhrRequest = {
+  makeRequest: function(url){
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Accept", "text/json");
+    xhr.onreadystatechange = this.receiveResponse;
+    xhr.send();
+  },
+  receiveResponse: function(){ //'this' is XMLHttpRequest
+    if (this.readyState == 4 && this.status == 200) {
+    console.log(this.statusText)
+    var requestedItems = JSON.parse(this.responseText);
+    currencyOptionsXhrRequest.formatResponse(requestedItems)
+  	}
+  },
+  formatResponse: function(el){
+  for (var key in el) {
+  if (el.hasOwnProperty(key)) {
+  	var baseCurrencyList = document.getElementById("baseCurrencyList");
+  	var comparisonCurrencyList = document.getElementById("comparisonCurrencyList");
+  	baseCurrencyList.innerHTML += "<option value='" + el[key].currency + "'>" + el[key].country + " " + el[key].currency + "</option>";
+  	comparisonCurrencyList.innerHTML += "<option value='" + el[key].currency + "'>" + el[key].country + " " + el[key].currency + "</option>";
+  	}
+   }
+  }
+};
+
 appController.initApp();
 
 
@@ -147,5 +237,4 @@ function initMap() {
 }
 initMap();
 */
-
 
